28b8f7d61a4f2e636ef087fb2f95d577d433d327
ankush06747-sketch <ankush06747@gmail.com>
2025-12-29 15:35:47 +0530

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -18,7 +18,7 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 
 # Page config
 st.set_page_config(page_title="Gemini Chat", page_icon="🤖")
-st.title("🤖 Gemini Multi-Turn Chat")
+st.title("🤖 AI Chatbot")
 
 # Initialize chat history
 if "chat" not in st.session_state:
@@ -68,6 +68,9 @@ if st.sidebar.button("🔄 Reset Conversation"):
     st.session_state.chat = model.start_chat(history=[])
     st.session_state.messages = []
     st.experimental_rerun()
+
+
+st.markdown('**Design and Developed by: Ankush Mishra**')
 
 
 
